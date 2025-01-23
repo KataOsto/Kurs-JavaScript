2f82b5ba24d2a80ac1b9160693abd55863b88808
kataosto@gmail.com <kataosto@gmail.com>
2025-01-23 08:18:01 +0100

    Demerit Points

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ function isLandscape(width, height) {
 
 
 // 12 FizzBuzz
+
 // Jeśli liczba jest dzielona przez 3 => Fizz
 // Jeśli liczba jest dzielona przez 5 => Buzz
 // Jeśli liczba jest dzielona przez 3 i 5 => FizzBuzz
 // Nie dzielona przez 3 i 5 => input
 // Nie jest numerem => 'Not a number'
 
+/*
 const  output = fizzBuzz(15);
 console.log(output);
 
@@ -46,4 +48,29 @@ function fizzBuzz(input) {
         return 'Buzz';
 
     return input;
+}
+*/
+   
+// 13 Demerit Points
+
+//Speed Limit = 70
+//5-> 1 point
+//use  Math.floor(1.3)
+//12 points -> suspended
+
+chackSpeed(130);
+
+function chackSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint)
+        console.log('Ok');
+    else {
+       const points = Math.floor((speed - speedLimit) / kmPerPoint);
+       if (points >= 12)
+        console.log('License suspended');
+       else
+        console.log('Points', points);
+    }
 }
