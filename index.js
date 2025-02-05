f4e1912be72152b15f748384640e2aea1ff9140a
kataosto@gmail.com <kataosto@gmail.com>
2025-02-05 08:44:20 +0100

    3 - Finding Elements(Primitive)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Arrays
 
 // 2 - Adding Elements
-
+/*
 // Fisrst decler a constsnt calld number -INFO const do not stop us from modyfing the array
 const numbers = [3, 4];
 
@@ -15,3 +15,23 @@ numbers.unshift(1, 2);
 //Middle -splice method
 numbers.splice(2, 0, 'a', 'b');
 console.log(numbers);
+*/
+
+
+// 3 - Finding Elements(Primitive)
+
+//Finding elements in an array depends on if we are storing primitives or reference types.
+
+const numbers = [1, 2, 3, 4];
+
+//IndexOf method - looking for an element in the array it returns the index of the element if not it returns "-1"
+
+console.log(numbers.indexOf(1)); 
+
+//Lastindexof - returns the last index, not it returns "-1"
+
+console.log(numbers.lastIndexOf(1));
+
+//Includes - returns true if element exist in the array
+
+console.log(numbers.includes(1));
