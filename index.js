fd7b5bef398b22ac1d120c85d5124be2f040903e
kataosto@gmail.com <kataosto@gmail.com>
2025-02-05 08:25:19 +0100

    2 - Adding Elements

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 //Arrays
 
+// 2 - Adding Elements
 
+// Fisrst decler a constsnt calld number -INFO const do not stop us from modyfing the array
+const numbers = [3, 4];
+
+// Methods defining  in arrays for adding new elemnts
+//End - push metod
+numbers.push(5, 6);
+
+//Beginning - unshift method
+numbers.unshift(1, 2);
+
+//Middle -splice method
+numbers.splice(2, 0, 'a', 'b');
+console.log(numbers);
