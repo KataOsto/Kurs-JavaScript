2f184666991da0253946d7bf15f4944689be81ec
kataosto@gmail.com <kataosto@gmail.com>
2025-02-14 07:59:05 +0100

    2 - Hoisting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Functions
 
 // 1 - Funtcions Declarations vs Expresions
-
+/*
 // In JavaScript we have tow ways to define a function, we can use the function decleration syntax
 // or function expression witch basically ivnoleves declaring a variable or a constant
 // amd then setting it to a function
@@ -27,3 +27,14 @@ let run = function () {
     console.log('run');
 };
 run();
+*/
+
+
+// 2 - Hoisting - proces of moving function to the top of the file - this is done automatically by the JavaScript
+
+// We can call a function that is define using the function decleration befor it definition// Funtcions Declarations 
+walk();
+
+function walk () {
+    console.log('walk');
+}
