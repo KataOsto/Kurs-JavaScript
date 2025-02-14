739c7383baec52b1693829deb109ca54aa30c4df
kataosto@gmail.com <kataosto@gmail.com>
2025-02-14 08:12:16 +0100

    3 - Arguments

--- index.js
+++ index.js
@@ -31,10 +31,39 @@ run();
 
 
 // 2 - Hoisting - proces of moving function to the top of the file - this is done automatically by the JavaScript
-
+/*
 // We can call a function that is define using the function decleration befor it definition// Funtcions Declarations 
 walk();
 
 function walk () {
     console.log('walk');
 }
+*/
+
+// 3 - Arguments
+
+//arguments is an array-like object accessible inside functions that contains the values of the arguments passed to that function
+function sum() {
+    let total = 0;
+    for (let value of arguments)
+        total += value;
+    return  total;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
